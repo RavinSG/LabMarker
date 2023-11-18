84f0a7e24f748fe97f1ae45641901f68f32c81a9
Ravin <ravinsg@gmail.com>
2023-11-19 10:20:20 +1100

    Included basic logging functionalities

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
-import colorlog
+import colorlog, logging
 from dataclasses import dataclass
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 @dataclass
@@ -21,6 +33,9 @@ class Config:
     paths: Paths
 
 
-handler = colorlog.StreamHandler()
+file_handler = logging.FileHandler('main.log', mode='w')
+file_formatter = logging.Formatter('%(levelname)-8s %(asctime)s:%(name)s:%(message)s')
+file_handler.setFormatter(file_formatter)
+
 logger = colorlog.getLogger('Marking')
-logger.setLevel("WARN")
+logger.addHandler(file_handler)
--- connection/ssh.py
+++ connection/ssh.py
@@ -1,17 +1,18 @@
 import paramiko
 from getpass import getpass
 
-from config import Config
+from config import Config, logger
 
 
 class Client:
     def __init__(self, cfg: Config):
         self.client = paramiko.SSHClient()
-
+        logger.info("Loading SSH known hosts")
         self.client.load_host_keys(cfg.paths.known_hosts)
         self.client.load_system_host_keys()
 
         if cfg.connection.use_pass:
+            logger.info("Waiting for user password")
             password = getpass("Enter password: ")
             self.client.connect(
                 hostname=cfg.connection.host_name,
@@ -20,16 +21,23 @@ class Client:
             )
         else:
             try:
+                logger.info("Loading private key")
                 key_file = paramiko.RSAKey.from_private_key_file(cfg.connection.private_key)
             except paramiko.PasswordRequiredException:
+                logger.warn("Key file requires password")
                 while True:
+                    logger.info("Waiting for key password")
                     try:
                         key_pass = getpass("Enter password for key file: ")
                         key_file = paramiko.RSAKey.from_private_key_file(cfg.connection.private_key, key_pass)
                         break
                     except paramiko.SSHException:
+                        logger.error("Entered key password is incorrect")
                         print("The entered key pass is invalid. Please try again.")
-
+            except FileNotFoundError:
+                logger.critical("Cannot load key file")
+                print("Cannot load the key file, please check the pathname")
+                exit(1)
             try:
                 self.client.connect(
                     hostname=cfg.connection.host_name,
@@ -37,10 +45,12 @@ class Client:
                     pkey=key_file
                 )
             except paramiko.AuthenticationException:
+                logger.critical("SSH authentication failed")
                 print("Authentication failed, please check whether password/key is correct.")
                 exit(1)
 
-        stdin, stdout, stderr = self.client.exec_command('ls')
+    def execute(self, command):
+        stdin, stdout, stderr = self.client.exec_command(command)
         print(stdout.readlines())
 
     def close(self):
